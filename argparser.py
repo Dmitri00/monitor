8689fe65c85dba7c98fc4707f2200c340f0a59cb
Dmitri00 <dima.toichkin@gmail.com>
2019-01-29 16:16:10 +0000

    initial commit of comanline arguments parser

---
+++ argparser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import argparse
+import sys
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('station', help='name of the station to monitor')
+parser.add_argument('-d', action='store_true', help='call monitor as daemon')
+parser.add_argument('-rm', action='store_true', help='don\'tsave recognized mp3 files')
+parser.add_argument('-loglevel',choices=('debug','info','error'),default='info', help='minimal level of log messages')
+parser.add_argument('-th',type=float,default='3', help='outlier threshold level')
+parser.add_argument('--track_len',type=int,choices=range(1,6),default='3', help='minimal observed track lenth for confident recognition')
+parser.add_argument('--period_len',type=int,choices=range(30,61),default='45', help='length of one record')
+
+
+cases = [ ['python3','new_main.py','test'],
+['python3','new_main.py','test', '-d','-loglevel','info','-rm','--period_len','30']
+]
+for case in cases:
+    print(parser.parse_args(case[2:]))
+print(parser.parse_args(sys.argv[1:]))
