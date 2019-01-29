e6d6f9b8a9473351c2dda4a2276c5f09c937f169
Dmitri00 <dima.toichkin@gmail.com>
2019-01-29 16:47:35 +0000

    error fixes to argparser

--- argparser.py
+++ argparser.py
@@ -4,7 +4,7 @@ import sys
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('station', help='name of the station to monitor')
 parser.add_argument('-d', action='store_true', help='call monitor as daemon')
-parser.add_argument('-rm', action='store_true', help='don\'tsave recognized mp3 files')
+parser.add_argument('-st', action='store_true', help='save recognized mp3 files')
 parser.add_argument('-loglevel',choices=('debug','info','error'),default='info', help='minimal level of log messages')
 parser.add_argument('-th',type=float,default='3', help='outlier threshold level')
 parser.add_argument('--track_len',type=int,choices=range(1,6),default='3', help='minimal observed track lenth for confident recognition')
@@ -12,7 +12,7 @@ parser.add_argument('--period_len',type=int,choices=range(30,61),default='45', h
 
 
 cases = [ ['python3','new_main.py','test'],
-['python3','new_main.py','test', '-d','-loglevel','info','-rm','--period_len','30']
+['python3','new_main.py','test', '-d','-loglevel','info','-st','--period_len','30']
 ]
 for case in cases:
     print(parser.parse_args(case[2:]))
